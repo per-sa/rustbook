34f371fcdfcb88552d02f92f5e7b285ee0852e24
clmb <ccolombomatheus@gmail.com>
2022-09-26 18:22:29 +0100

    Delete microsofttraining/target directory

--- microsofttraining/target/debug/deps/microsofttraining.d
+++
@@ -1,5 +0,0 @@
-C:\Users\matheus\Desktop\personaldev\rustbook\microsofttraining\target\debug\deps\microsofttraining.exe: src\main.rs
-
-C:\Users\matheus\Desktop\personaldev\rustbook\microsofttraining\target\debug\deps\microsofttraining.d: src\main.rs
-
-src\main.rs:
--- microsofttraining/target/debug/microsofttraining.d
+++
@@ -1,1 +0,0 @@
-C:\Users\matheus\Desktop\personaldev\rustbook\microsofttraining\target\debug\microsofttraining.exe: C:\Users\matheus\Desktop\personaldev\rustbook\microsofttraining\src\main.rs
